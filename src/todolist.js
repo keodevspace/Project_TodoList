d5ee8be63a1b1f9c31fbb37228d06c5ea03c76f9
Cleiton Sena <cleitonsena21@hotmail.com>
2024-02-14 21:46:08 -0300

    codigo comitado para encontrarmos uma solucao: trabalhar individualmente na solucao

--- src/todolist.js
+++ src/todolist.js
@@ -6,4 +6,28 @@
 // - Listar todas as tarefas salvas
 // - Obter uma tarefa, através de um parâmetro (id)
 
-let adicionar = 0;
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form');
+    const list = document.getElementById('task-list');
+
+
+
+
+let formulario = document.querySelector('form');
+let tituloDaTarefa = document.getElementById('task-title');
+let detalhesDaTarefa = document.getElementById('task-details');
+let botaoGuardar = document.getElementById('btn-store');
+
+formulario.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    if (tituloDaTarefa === "" || detalhesDaTarefa === "") {
+        alert('Por favor, preencher todos os campos.');
+        return;
+    } 
+    
+    console.log(botaoGuardar);
+    
+});
+
+});
