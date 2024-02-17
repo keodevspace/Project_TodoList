bfd665a37d11952c852377edd0d38f5e45cc8edb
arthurcaaldas <arthurcaaldas@hotmail.com>
2024-02-17 16:07:53 -0300

    resolvendo conflitos entre animacao e botao switch, html ajustado

--- src/todolist.js
+++ src/todolist.js
@@ -1,61 +1,75 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const taskTitleInput = document.getElementById("task-title");
-  const taskDetailsInput = document.getElementById("task-details");
-  const addTaskBtn = document.getElementById("btn-store");
-  const taskList = document.getElementById("taks-list");
+document.addEventListener("DOMContentLoaded", function() {
+    const taskTitleInput = document.getElementById('task-title');
+    const taskDetailsInput = document.getElementById('task-details');
+    const addTaskBtn = document.getElementById('btn-store');
+    const taskList = document.getElementById('taks-list');
+    
+    addTaskBtn.addEventListener('click', function(event) {
+        event.preventDefault();
+        
+        const taskTitle = taskTitleInput.value.trim();
+        const taskDetails = taskDetailsInput.value.trim();
+        
+        if (taskTitle !== '' && taskDetails !== '') {
+            const listItem = document.createElement('div');
+            listItem.className = 'list-group-item mb-2 flex-column align-items-start bg-dark text-white border-info-subtle form-check';
+            
+            const titleElement = document.createElement('h5');
+            titleElement.className = 'mb-1';
+            titleElement.textContent = taskTitle;
+            
+            const detailsElement = document.createElement('p');
+            detailsElement.className = 'mb-1 py-4';
+            detailsElement.textContent = taskDetails;
+            
+            const editBtn = document.createElement('button');
+            editBtn.className = 'btn btn-secondary';
+            editBtn.textContent = 'Editar';
+            editBtn.addEventListener('click', function() {
+                const newTaskTitle = prompt('Editar título:', taskTitle);
+                const newTaskDetails = prompt('Editar detalhes:', taskDetails);
+                if (newTaskTitle !== null && newTaskDetails !== null) {
+                    titleElement.textContent = newTaskTitle;
+                    detailsElement.textContent = newTaskDetails;
+                }
+            });
+            
+            const removeBtn = document.createElement('button');
+            removeBtn.className = 'btn btn-secondary m-2';
+            removeBtn.textContent = 'Remover';
+            removeBtn.addEventListener('click', function() {
+                listItem.classList.add('animate__animated', 'animate__backOutDown');
+                setTimeout(function() {
+                    listItem.remove();
+                }, 1000);
+            });
 
-  addTaskBtn.addEventListener("click", function (event) {
-    event.preventDefault();
+            const switchContainer = document.createElement('div');
+            switchContainer.className = "mt-3 float-end";
+            const switchLabel = document.createElement('label');
+            switchLabel.className = "form-check-label";
+            switchLabel.textContent = "Feito";
+            const switchBtn = document.createElement('input');
+            switchBtn.className = "p-2 form-check-input form-switch";
+            switchBtn.setAttribute('type', 'checkbox');
+            switchBtn.setAttribute('role', 'switch');
+            switchBtn.setAttribute("id", "flexSwitchCheckDefault");
 
-    const taskTitle = taskTitleInput.value.trim();
-    const taskDetails = taskDetailsInput.value.trim();
+            switchContainer.appendChild(switchLabel);
+            switchContainer.appendChild(switchBtn);
 
-    if (taskTitle !== "" && taskDetails !== "") {
-      const listItem = document.createElement("div");
-      listItem.className =
-        "list-group-item mb-2 flex-column align-items-start bg-dark text-white border-info-subtle animate__animated";
-
-      const titleElement = document.createElement("h5");
-      titleElement.className = "mb-1";
-      titleElement.textContent = taskTitle;
-
-      const detailsElement = document.createElement("p");
-      detailsElement.className = "mb-1 py-4";
-      detailsElement.textContent = taskDetails;
-
-      const editBtn = document.createElement("button");
-      editBtn.className = "btn btn-secondary";
-      editBtn.textContent = "Editar";
-      editBtn.addEventListener("click", function () {
-        const newTaskTitle = prompt("Editar título:", taskTitle);
-        const newTaskDetails = prompt("Editar detalhes:", taskDetails);
-        if (newTaskTitle !== null && newTaskDetails !== null) {
-          titleElement.textContent = newTaskTitle;
-          detailsElement.textContent = newTaskDetails;
+            listItem.appendChild(titleElement);
+            listItem.appendChild(detailsElement);
+            listItem.appendChild(editBtn);
+            listItem.appendChild(removeBtn);
+            listItem.appendChild(switchContainer);
+            
+            taskList.appendChild(listItem);
+            
+            taskTitleInput.value = '';
+            taskDetailsInput.value = '';
+        } else {
+            alert('Por favor, preencha todos os campos.');
         }
-      });
-
-      const removeBtn = document.createElement("button");
-      removeBtn.className = "btn btn-secondary m-2";
-      removeBtn.textContent = "Remover";
-      removeBtn.addEventListener("click", function () {
-        listItem.classList.add("animate__backOutDown");
-        setTimeout(function () {
-          listItem.remove();
-        }, 2000);
-      });
-
-      listItem.appendChild(titleElement);
-      listItem.appendChild(detailsElement);
-      listItem.appendChild(editBtn);
-      listItem.appendChild(removeBtn);
-
-      taskList.appendChild(listItem);
-
-      taskTitleInput.value = "";
-      taskDetailsInput.value = "";
-    } else {
-      alert("Por favor, preencha todos os campos.");
-    }
-  });
+    });
 });
